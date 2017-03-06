c600c9a963671ff1f547e3f843784db32e1e1594
Timmie Sarjanen <timmie.sarjanen@gmail.com>
2017-03-06 17:47:46 +0100

    Some more

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-'use strict';
-
 const FastBootAppServer = require('fastboot-app-server');
-
-let server = new FastBootAppServer({
+const S3Notifier = require('fastboot-s3-notifier');
+const S3Downloader = require('fastboot-s3-downloader');
+const Creds = {
   distPath: 'dist',
-  gzip:     true
-});
+  bucket: 'vanligajavlarecept',
+  key: 'dist.zip'
+};
 
-server.listen(8080);
+let downloader = new S3Downloader(Creds);
+let notifier = new S3Notifier(Creds);
+let server = new FastBootAppServer({
+  downloader,
+  notifier
+});
 
 server.start();
